e8e862955e7f20ec6f3bff14e02466f579a893d0
Eric Veenendaal <eric@veenendaal.co>
2023-01-28 08:17:39 +0100

    Limiting cleanup to just this app

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT="veenendaal-base"
+REGION="europe-west4"
+
+for LINE in $(gcloud run revisions list --region="$REGION" --filter="status.conditions[1].status != True AND metadata.ownerReferences.name = 'videogallery'" --format="value(metadata.name)"); do
+  gcloud run revisions delete --region=$REGION --quiet --async "$LINE" || true
+done
+
+for LINE in $(gcloud artifacts docker images list "$REGION-docker.pkg.dev/$PROJECT/docker/videogallery" --format="value[separator='@']( package,version)"); do
+  gcloud artifacts docker images delete --quiet --async "$LINE" || true
+done

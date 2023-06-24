1e58f183b5adbd2ff0af4d68567f93a058680335
Eric Veenendaal <eric@ericveenendaal.com>
2023-06-24 23:04:38 +0200

    cleanup

--- app.ts
+++ app.ts
@@ -4,6 +4,9 @@ import * as crypto from 'crypto'
 import {File} from "@google-cloud/storage/build/src/file";
 import * as path from "path";
 
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
 const app = express()
 
 app.disable('x-powered-by')
@@ -33,6 +36,8 @@ interface Video {
   thumbnail?: string
 }
 
+type Galleries = Record<string, Gallery[]>
+
 function generateSecret(stub: string): string {
   const md5Hasher = crypto.createHmac('md5', 'QuxFzI9lcmwfcg')
   return md5Hasher.update(stub).digest('base64url').slice(0, 4)
@@ -53,43 +58,60 @@ async function getGalleries(): Promise<Gallery[]> {
   // Sign Urls
   const galleries = [];
   for (const videoFile of videoFiles) {
-    const thumbnailFilename = path.parse(videoFile.name).base + ".jpg"
+    let parsedPath = path.parse(videoFile.name);
+    const thumbnailFilename = parsedPath.name + ".jpg"
 
     // Create Stub
-    const categoryStub = videoFile.group = videoFile.group
+    const categoryStub = videoFile.group
       .replace(/\s+/g, '-')
       .replace(/[^a-zA-Z0-9-_]/g, '')
       .toLowerCase()
-    const stub = `${generateSecret(categoryStub)}/${categoryStub}`
+    const stub = `/${generateSecret(categoryStub)}/${categoryStub}`
 
-    galleries.push({
+    // Create Gallery
+    let videoFileName = await signUrl(`${videoFile.filename}`);
+    let thumbnailFileName = await signUrl(`${videoFile.category}/${videoFile.group}/thumbnails/${thumbnailFilename}`);
+
+    const gallery = {
       name: videoFile.group,
       category: videoFile.category,
       stub: stub,
       videos: [{
         name: videoFile.name,
-        url: await signUrl(`https://storage.googleapis.com/${bucketName}/${videoFile.filename}`) ?? "",
-        thumbnail: await signUrl(`https://storage.googleapis.com/${bucketName}/${videoFile.category}/${videoFile.group}/thumbnails/${thumbnailFilename}`)
+        url: videoFileName,
+        thumbnail: thumbnailFileName
       } as Video]
-    } as Gallery);
+    } as Gallery
+
+    galleries.push(gallery);
   }
 
-  const finalGalleries = galleries.reduce((accumulator: Gallery[],
+  return galleries.reduce((accumulator: Gallery[],
                            current: Gallery) => {
     let gallery: Gallery | undefined = accumulator.find(gallery => gallery.name == current.name)
     if (gallery == null) {
       gallery = current
       accumulator.push(gallery)
+    } else {
+      current.videos?.forEach(video => {
+        gallery?.videos?.push(video)
+      })
     }
-    current.videos?.forEach(video => {
-      gallery?.videos?.push(video)
-    })
     return accumulator
   }, [])
+}
 
-  console.log(finalGalleries)
-
-  return finalGalleries
+function toDisplay(galleries: Gallery[]): Galleries {
+  const result = {} as Galleries
+  galleries.forEach(gallery => {
+    const stub = gallery.category!
+    if (!result[stub]) {
+      result[stub] = [gallery]
+    } else {
+      result[stub].push(gallery)
+    }
+  })
+  return result
 }
 
 app.get('/', async (req: Request, res: Response): Promise<void> => {
@@ -102,41 +124,45 @@ app.get('/feed', async (req: Request, res: Response) => {
 
 app.get('/TWs0/index', async (req: Request, res: Response): Promise<void> => {
   res.render('index', {
-    galleries: await getGalleries()
+    galleries: toDisplay(await getGalleries())
   })
 })
 
-async function signUrl(prefix: string): Promise<string | null> {
+async function signUrl(filename: string): Promise<string | null> {
   async function signFile(file: File): Promise<string | null> {
     const response = await file.getSignedUrl({
       action: 'read',
       expires: Date.now() + 1000 * 60 * 60 * 24 // one day
     }).catch(error => {
       console.error(error)
-    }) ?? null
+    })
 
     return response?.[0] ?? null
   }
 
-  let response = await bucket.getFiles({prefix: `${prefix}/`, delimiter: '/'});
+  let cache = await myCache.get(filename);
 
-  const files = await response
-    .filter(file => file.getSignedUrl !== undefined)
-    .map(async (file: File) => await signFile(file))
-    .pop()
-
-  return files!!
+  if (cache) {
+    return cache.result;
+  } else {
+    const file = await bucket.file(filename);
+    const fileExists = (await file.exists())[0]
+    const result = fileExists ? (await signFile(file)) : null
+    myCache.set(filename, {
+      fileExists: fileExists,
+      result: result
+    }, 600)
+    return result
+  }
 }
 
 app.get('/:password/:gallery', async (req: Request, res: Response): Promise<void> => {
-  const stub = req.params.gallery
-  const galleries = await getGalleries()
+  const stub = `/${req.params.password}/${req.params.gallery}`
+  const galleries = (await getGalleries())
   const gallery = galleries.find(gallery => gallery.stub === stub)
 
-  if (generateSecret(stub) === req.params.password) {
-    res.render('gallery', {
-      gallery
-    })
+  if (gallery) {
+    res.render('gallery', gallery)
   } else {
     res.status(404).send()
   }

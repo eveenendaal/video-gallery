7ee0ebc41078090c65cac065787f01f675ba6efa
Eric Veenendaal <eric@ericveenendaal.com>
2026-01-17 22:30:42 +0100

    Update admin.pug to use dynamic secret key and improve video management functionality

--- pkg/handlers/admin_handlers.go
+++ pkg/handlers/admin_handlers.go
@@ -34,10 +34,12 @@ func AdminHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = template.Execute(w, models.Admin{
+	adminData := models.Admin{
 		Categories: services.GetCategories(),
 		SecretKey:  secretKey,
-	})
+	}
+	log.Printf("Admin SecretKey: %s", adminData.SecretKey)
+	err = template.Execute(w, adminData)
 
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Template execution error: %v", err), http.StatusInternalServerError)
